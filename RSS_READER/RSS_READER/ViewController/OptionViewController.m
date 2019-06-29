a0201eda43a163b3ebddc8a2873032a71087b82f
SonPham <phamngocsonl13.cla@gmail.com>
2019-06-30 00:09:11 +0700

    Thêm chức năng select Mode trong Setting
    Người đăng : Phạm Ngọc Sơn

--- RSS_READER/RSS_READER/ViewController/OptionViewController.m
+++ RSS_READER/RSS_READER/ViewController/OptionViewController.m
@@ -9,6 +9,11 @@
 #import "OptionViewController.h"
 
 @interface OptionViewController ()
+@property (strong, nonatomic) IBOutlet UIView *MainView;
+@property (strong,nonatomic) UIColor *darkMode;
+@property (strong,nonatomic) UIColor *whiteMode;
+@property (weak, nonatomic) IBOutlet UILabel *labelSelectMode;
+@property (weak, nonatomic) IBOutlet UILabel *labelSelectFontSize;
 
 @end
 
@@ -16,7 +21,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _whiteMode = [UIColor whiteColor];
+    _darkMode = [UIColor grayColor];
     // Do any additional setup after loading the view.
+}
+//Function: Select Dark Mode
+- (IBAction)SelectDarkMode:(UIButton *)sender {
+    _MainView.backgroundColor = _darkMode;
+    _labelSelectMode.textColor = _whiteMode;
+    _labelSelectFontSize.textColor = _whiteMode;
+}
+//Function: Select White Mode
+- (IBAction)SelectWhiteMode:(UIButton *)sender {
+    _MainView.backgroundColor = _whiteMode;
+    _labelSelectMode.textColor = _darkMode;
+    _labelSelectFontSize.textColor = _darkMode;
 }
 
 /*
